edbb3121ec741906e1dc96cdadc1ae815514038e
raflesngln <raflesngln@gmail.com>
2016-01-23 12:10:35 +0700

    add struk

--- application/views/barcode/laporan_karyawan.php
+++ application/views/barcode/laporan_karyawan.php
@@ -23,13 +23,20 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>.</title>
 <style>
+*{font-size:10px;}
 P{ margin-top:-4PX;}
 
-table{
-	width:80%;
+#mytable{
+	width:99%;
 	margin-top:9px;
+	border:1px #666 solid;
+}
+#mytable tr td{
+	border-left:1px #666 solid;
+	border-bottom:1px #666 solid;
 }
 footer{display:none;}
+
 </style>
 </head>
 
@@ -41,54 +48,155 @@ footer{display:none;}
     <?php foreach($connote as $data){
 
 		 ?>
-<table width="" border="1">
+<table width="86%" border="0" id="mytable">
   <tr>
-    <td width="56"><p>XSYS</p>
+    <td width="134" rowspan="2"><p>XSYS</p>
       <p>EXpress Network</p></td>
-    <td width="42"><p>PT.Expresindo System Network</p>
+    <td width="135" rowspan="2"><p>PT.Expresindo System Network</p>
       <p>perkantoran Galaxy Blok N-27</p>
       <p>Outer Ring Road Barat</p>
       <p>Cengkareng-Jakarta Barat 11730</p></td>
-    <td width="45"><p>ORIGINAL/ASAL</p>
+    <td width="154"><p>ORIGINAL/ASAL</p>
       <p><?php echo $data->Origin;?></p></td>
-    <td width="159"><p>DESTINATION/TUJUAN</p>
+    <td width="200"><p>DESTINATION/TUJUAN</p>
       <p><?php echo $data->Destination;?></p></td>
-    <td width="159"><p>|||||||||||||||||||||</p>
+    <td colspan="2" rowspan="2"><p>|||||||||||||||||||||</p>
       <p>&nbsp;</p>      <?php echo $data->HouseNo;?></td>
+    </tr>
+  <tr>
+    <td><p>PIECES/JUMLAH SATUAN</p>
+      <p><?php echo $data->grandPCS;?></p></td>
+    <td><p>WEIGHT/BERAT</p>
+      <p><?php echo $data->GrossWeight;?></p></td>
   </tr>
   <tr>
-    <td>Account No.</td>
-    <td>dfgdgd</td>
-    <td>gdgdgd</td>
-    <td>fdggf</td>
-    <td>dsfdsf</td>
+    <td colspan="2">Account No.</td>
+    <td colspan="2" rowspan="3">LBy giving us shipment you agree of all itemsof conditions of nonNegoitable Connote /Dengan menyerahkan kiriman anda setuju dengan ketentuan dan kondisi pada nota pengiriman ini tanpa syarat, termasuk kondisi dan ketentuan yang tertera pada bagian belakang.menyerahkan kiriman anda setuju dengan kondisi dan ketentuan pada nota ini.</td>
+    <td colspan="2">DECLARE VALUE/NILAI KIRIMAN</td>
+    </tr>
+  <tr>
+    <td>TYPE OF PAYMENT/JENIS PEMABAYARAN</td>
+    <td>TYPE OF SHIPMENT/JENIS KIRIMAN</td>
+    <td width="102" rowspan="2">SERVICE/LAYANAN</td>
+    <td width="17" rowspan="2">CHARGES/HARGA (IDR)</td>
   </tr>
   <tr>
-    <td>fdgdg</td>
-    <td>dfgdg</td>
-    <td>dfg</td>
-    <td>dfgdgdfgg</td>
+    <td><input type="checkbox" />
+CASH
+  <label for="select12"></label>
+  <input type="checkbox" />
+CREDIT
+<label for="select13"></label>
+<input type="checkbox" />
+COD </td>
+    <td><input type="checkbox" />
+      DOC
+      <label for="select14"></label>
+      <input type="checkbox" />
+      NDX
+      <label for="select15"></label>
+      <input type="checkbox" />
+      HVS</td>
+    </tr>
+  <tr>
+    <td colspan="2">
+     <?php
+	foreach($shipper as $ship){
+	?>
+    <p>SHIPMENT/PENGIRIM</p>
+    <p><?php echo $ship->custName;?></p>
+      <p>ADDRESS/ALAMAT</p>
+      <p><?php echo $ship->Address;?></p>      
+      <label for="select7"><br />
+    </label>
+    <?PHP } ?>
+    </td>
+    <td colspan="2">
+<?php
+	foreach($consigne as $con){
+	?>
+    <p>RECEIVE/PENERIMA</p>
+    <p><?php echo $con->custName;?></p>
+      <p>ADDRESS/ALAMAT</p>
+      <p><?php echo $con->Address;?></p>
+      <p>&nbsp;</p>
+ <?PHP } ?> 
+      </td>
+    <td colspan="2"><table  width="100%" border="0" style="border:none; margin-top:-68px; width:300px">
+    <?php foreach($charges as $row){
+		$unit=$row->Unit;
+		$qty=$row->Qty;
+		$total=$unit*$qty;
+		$grantotal+=$total;
+		 ?>
+  <tr>
+    <td width="150"><?php echo $row->ChargeName;?></td>
+    <td width="60"><div align="right" style="border:1px #DAD5D5 solid; text-align:right; float:right"><label style="color:#FFF">........</label><?php echo number_format($total,0,'.',',');?></div></td>
+  </tr>
+  <?php } ?>
+</table></td>
+    </tr>
+  <tr>
+    <td colspan="2"><span class="detsend"> SHIPPER SIGNATURE/TANDA TANGAN PENGIRIM : </span></td>
+    <td colspan="2"><span class="detsend"> ATTENTION &amp; DEPT/DITUJUKAN &amp; DEPT: </span></td>
+    <td>TOTAL/JUMLAH</td>
+    <td><label style="color:#FFF">...</label>
+      &nbsp;Rp.<?php echo number_format($grantotal,0,',','.');?>
+      </label></td>
+  </tr>
+  <tr>
+    <td colspan="2"><span class="detsend">DESCRIPTION OF SHIPMENT/KETERANGAN ISI : <?php echo $data->DescofShipment;?></span></td>
+    <td colspan="2"><span class="detsend">COLLECTED BY X-SYS/DIAMBIL OLE X-SYS</span></td>
+    <td colspan="2">   <label style="color:#F00">PERNYATAN PENGIRIMAN :</label>
+        <p style="color:#F00">Kami memahami dan menyetujiui bahwa kiriman senilai Rp 1.000.000,- atau lebih harus di asuransikan. Jika tidak di asuransikan, </p>
+      </td>
+    </tr>
+  <tr>
+    <td colspan="2"><span class="detsend">DIMENTION/DIMENSI BARANG : <?php echo $data->grandVolume;?></span></td>
+    <td colspan="2"><span class="detsend">COLLECTED BY X-SYS/DIAMBIL OLE X-SYS</span></td>
+    <td>&nbsp;</td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td>dfgd</td>
-    <td>dfgdg</td>
+    <td colspan="2"><span class="detsend">SPECIAL INTRUCTION//INSTRUKSI KHUSUS : <?php echo $data->SpecialIntraction;?></span></td>
+    <td>DATE/TANGGAL :<?php echo date("d-m-Y",strtotime($data->ETD)); ?></td>
+    <td>TIME/JAM :<?php echo date("h:i:s",strtotime($data->ETD)); ?></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2">&nbsp;</td>
+    <td colspan="2"><p style="font-size:16pt; text-align:center">WE CANNOT DELIVER TO PO.BOX</p>
+      <p style="font-size:16pt; text-align:center"><span class="detsend">Kami tidak dapat mengantar PO BOX</span></p></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2">&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="2">dfgddfgdg</td>
     <td>dfggdg</td>
     <td>dfgd</td>
+    <td>&nbsp;</td>
     <td>&nbsp;</td>
   </tr>
 </table>
 <?php } ?>
 </div>
 
-<div class="row">
-<div class="col-sm-3">jakarta barat</div>
-<div class="col-sm-3">jakarta barat</div>
-<div class="col-sm-3">jakarta barat</div>
+
+
+<div class="row text-center">
+<div class="col-sm-12"><h1>&nbsp;</h1></div>
+
+<button class="btn btn-primary btn-large btn-lg" onclick="printContent('nama')"><i class="fa fa-print fa-2x"></i> Print Connote</button>
 </div>
 
-
-<button onclick="printContent('nama')">Print</button>
 </div>
 
 
